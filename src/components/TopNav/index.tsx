4529ff9ec97e281f33be9676b3a27b6d35856ac7
prakhar.pratap <prakhar.pratap@diatoz.com>
2024-12-13 19:31:36 +0530

    solum and customer toggle button added in top navbar

--- src/components/TopNav/index.tsx
+++ src/components/TopNav/index.tsx
@@ -1,10 +1,20 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
+import Buttons from "../../design-system/designComponents/Buttons/Button";
 import { Avatar } from "@mui/material";
 import Textfield from "../../design-system/designComponents/Textfield";
 import "./style.scss";
 
 const TopNav = () => {
   const [search, setSearch] = useState("");
+  // const [viewType, setViewType] = useState("solum-view");
+  const [viewType, setViewType] = useState(() => {
+    return localStorage.getItem("viewType") || "solum-view";
+  });
+
+  useEffect(() => {
+    localStorage.setItem("viewType", viewType);
+  }, [viewType]);
+
   return (
     <div id="top_nav_wrapper">
       <div className="nav-contents">
@@ -16,6 +26,21 @@ const TopNav = () => {
             }
             placeholder="Search here..."
             type="search"
+          />
+        </div>
+
+        <div
+          className="d-flex align-items-center"
+          style={{ columnGap: "10px" }}
+        >
+          <Buttons
+            title="Solu-M View"
+            onClick={() => setViewType("solum-view")}
+          />
+          <Buttons
+            title="Customer View"
+            variant="outlined"
+            onClick={() => setViewType("customer-view")}
           />
         </div>
         <div className="profile">
